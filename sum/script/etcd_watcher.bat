569bc3eeccafb394e8f34b064017d7f9edb3fd90
unknown <yuandanshui@lilith.com>
2022-09-22 19:58:00 +0800

    rm old

--- sum/script/etcd_watcher.bat
+++
@@ -1,3 +0,0 @@
-docker pull joinsunsoft/etcdv3-browser:0.9.0
-docker run -itd --rm --name=etcdv3-browser -p 9980:80 joinsunsoft/etcdv3-browser:0.9.0
-@pause
--- sum/sumpb/gen.bat
+++
@@ -1,5 +0,0 @@
-protoc -I. --go_out=plugins=grpc:. sum.proto
-
-#env
-#go get -u github.com/golang/protobuf/protoc-gen-go@v1.3.2
-#go install github.com/golang/protobuf/protoc-gen-go
